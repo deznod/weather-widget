dbc334b1f208955b18b30f14ddeb0b72c66e31d9
deznod <fredomworld1@gmail.com>
2018-10-07 20:32:00 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $.getJSON('current.city.list.json',function(data){
+        $('select').on('change', function(){
+            var out=' ';
+            for(var key in data){
+                if(data[key].country==$('select option:selected').val()){
+                    out += '<p value="'+data[key].id+'">'+data[key].name+'</p>';
+
+                }
+
+            }
+            $('#city').html(out);
+            $('#city p').on('click', function(){
+                $.get(
+                    "http://api.openweathermap.org/data/2.5/weather",
+    {
+        "id" : $(this).attr('value'),
+        "appid": "d6fadff7b900b59b90336186e3a03a60"
+    },
+    function (data){
+        let out='';
+        out +='Погода: <b>'+data.weather[0].main+'</b><br>';
+        out +='<p style ="text-align:center"><img src="https://openweathermap.org/img/w/'+data.weather[0].icon+'.png"</p>';
+        out +='Температура: <b>'+Math.round(data.main.temp-273)+'&#176;C</b><br>';
+        out +='Влажность: <b>'+data.main.humidity+'%</b><br>';
+        console.log(data.main);
+        $('#weather').html(out);
+    }
+                );  
+            });
+        
+
+            
+        
+    });
+});
+
+});
