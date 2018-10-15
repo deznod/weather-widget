8aebfaa50924035014e8d4e4cd308d65a0c305f2
deznod <fredomworld1@gmail.com>
2018-10-15 04:12:29 +0300

    добавлена возможность просмотра погоды на 5 дней вперед

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
                             'Влажность: <b>' + data.main.humidity + '%</b><br>' +
                             'Давление: <b>' + Math.round(data.main.pressure * 0.00750063755419211 * 100) + ' мм.рт.ст</b><br>' +
                             'Видимость: <b>' + data.visibility / 1000 + ' км</b><br>' +
-                            'Город:<b>' + data.name + '</br>' +
+                            'Город:<b>' + data.name + '</b></br>' +
                             'Время:<b>' + Hour + ':' + Minutes + ':' + Seconds;
                         console.log(data.main);
                         $('#weather').html(out);
@@ -73,10 +73,48 @@ function showPosition(position) {
                 'Давление: <b>' + Math.round(data.main.pressure * 0.00750063755419211 * 100) +
                 ' мм.рт.ст</b><br>' +
                 'Видимость: <b>' + data.visibility / 1000 + ' км</b><br>' +
-                'Город:<b>' + data.name + '</br>' +
+                'Город:<b>' + data.name + '</b></br>' +
                 'Время:<b>' + Hour + ':' + Minutes + ':' + Seconds;
 
             console.log(data.main);
+            $('#weather').html(out);
+        }
+    );
+}
+function getLocationWeather() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPositionWeather);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPositionWeather(position) {
+    x.innerHTML = "Широта: " + position.coords.latitude +
+        "<br>Долгота: " + position.coords.longitude;
+    $.get(
+        "http://api.openweathermap.org/data/2.5/forecast?lat=" + position.coords.latitude + "&lon=" +
+        position.coords.longitude, {
+            // "id": $(this).attr('value'),
+            "appid": "d6fadff7b900b59b90336186e3a03a60"
+        },
+        function (data) {
+            let out = '';
+           let i;
+            for(i=0;i<39;i++){
+                
+            out += 'Погода: <b>' + data.list[i].weather[0].main + '</b>' +
+                '<img src="https://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png"' +
+                'Температура: <b>' + Math.round(data.list[i].main.temp - 273) + '&#176;C</b><br>' +
+                'Влажность: <b>' + data.list[i].main.humidity + '%</b><br>' +
+                'Давление: <b>' + Math.round(data.list[i].main.pressure * 0.00750063755419211 * 100) +
+                ' мм.рт.ст</b><br>' +
+                'Видимость: <b>' + data.list[i].visibility / 1000 + ' км</b><br>' +
+                'Город:<b>' + data.city.name + '</b></br>' +
+                'Дата и время погоды: <b>'+data.list[i].dt_txt+'</b> <br>'+
+                'Текущее Время:<b>' + Hour + ':' + Minutes + ':' + Seconds+'</b><br>';
+            }
+            console.log(data);
+            
             $('#weather').html(out);
         }
     );
